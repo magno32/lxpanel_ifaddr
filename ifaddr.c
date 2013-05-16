0b2cd5f0aaa095b27ae7837fffe8cc36d4125bff
Justin Smith <magno32@gmail.com>
2013-05-16 11:19:26 -0500

    Patch from Angelantonio Pacucci that shows all network interfaces.

--- ifaddr.c
+++ ifaddr.c
@@ -32,14 +32,13 @@ typedef struct
 static int 
 ifaddr_constructor(Plugin * p, char ** fp)
 {
+
  /* get the ip... */
-
-char ifName[16] = "UNK";
-char ifAddr[128] = "UNKNOWN";
-
 struct ifaddrs *ifaddr, *ifa;
     int s;
     char host[NI_MAXHOST];
+    int interfaces;
+    char MycIdBuf[256] = {'\0'};
 
     if (getifaddrs(&ifaddr) == -1) 
     {
@@ -47,27 +46,32 @@ struct ifaddrs *ifaddr, *ifa;
         exit(EXIT_FAILURE);
     }
 
-
+    interfaces=0;
     for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) 
     {
         if (ifa->ifa_addr == NULL)
             continue;  
-
         s=getnameinfo(ifa->ifa_addr,sizeof(struct sockaddr_in),host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-
-        if((strcmp(ifa->ifa_name,"eth0")==0)&&(ifa->ifa_addr->sa_family==AF_INET))
+        if (ifa->ifa_addr->sa_family==AF_INET)
+                                             interfaces=interfaces+1;
+        if(ifa->ifa_addr->sa_family==AF_INET)
         {
             if (s != 0)
             {
                 printf("getnameinfo() failed: %s\n", gai_strerror(s));
                 exit(EXIT_FAILURE);
             }
-            
-	    strcpy(ifName, ifa->ifa_name);
-	    strcpy(ifAddr, host);
-	    break;
+        if((strcmp(ifa->ifa_name,"lo")!=0)&&(ifa->ifa_addr->sa_family==AF_INET))  
+            {          
+            strcat(MycIdBuf,ifa->ifa_name);
+            strcat(MycIdBuf,": ");
+            strcat(MycIdBuf,host);
+            strcat(MycIdBuf,"\n");
+            } 
+	    /* break; */
         }
     }
+
 
     freeifaddrs(ifaddr);
 /* done getting ip... */
@@ -89,7 +93,9 @@ struct ifaddrs *ifaddr, *ifa;
  // make a label out of the ID
  char cIdBuf[256] = {'\0'};
 
- sprintf(cIdBuf, "%s: %s",ifName, ifAddr);
+ /*sprintf(cIdBuf, "%s: %s %d",ifName, ifAddr, interfaces);*/
+ strcpy(cIdBuf,MycIdBuf);
+
 
  GtkWidget *pLabel = gtk_label_new(cIdBuf);
 
@@ -103,7 +109,7 @@ struct ifaddrs *ifaddr, *ifa;
  gtk_container_add(GTK_CONTAINER(p->pwid), GTK_WIDGET(pLabel));
 
  // set the size we want
- gtk_widget_set_size_request(p->pwid, 148, 25);
+ gtk_widget_set_size_request(p->pwid, 158, 25);
 
  // our widget doesn't have a window...
  gtk_widget_set_has_window(p->pwid, FALSE);
@@ -150,9 +156,9 @@ PluginClass ifaddr_plugin_class = {
 
    // type of this plugin
    type : "ifaddr",
-   name : N_("Interface Address"),
+   name : N_("Interface Address List"),
    version: "1.0",
-   description : N_("Show the IP Address of eth0."),
+   description : N_("Show the IP Address of network interfaces."),
 
    // we can have many running at the same time
    one_per_system : FALSE,
@@ -166,3 +172,4 @@ PluginClass ifaddr_plugin_class = {
    config : ifaddr_configure,
    save : ifaddr_save_configuration
 };
+
